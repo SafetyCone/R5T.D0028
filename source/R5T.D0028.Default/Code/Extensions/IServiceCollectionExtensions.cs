3fbcdc0069c589757d094bb893b6b64ee27eed25
MinexAutomation <minex.automation@gmail.com>
2020-07-04 23:07:59 -0700

    Added R5T.D0028.Default library. Added MachineNameProvider service implementation class type.

---
+++ source/R5T.D0028.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using IStringlyTypedMachineNameProvider = R5T.D0027.IMachineNameProvider;
+
+
+namespace R5T.D0028.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add <see cref="MachineNameProvider"/> implementation of <see cref="IMachineNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddMachineNameProvider(this IServiceCollection services,
+            IServiceAction<IStringlyTypedMachineNameProvider> stringlyTypedMachineNameProviderAction)
+        {
+            services
+                .AddSingleton<IMachineNameProvider, MachineNameProvider>()
+                .Run(stringlyTypedMachineNameProviderAction)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add <see cref="MachineNameProvider"/> implementation of <see cref="IMachineNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IMachineNameProvider> AddMachineNameProviderAction(this IServiceCollection services,
+            IServiceAction<IStringlyTypedMachineNameProvider> stringlyTypedMachineNameProviderAction)
+        {
+            var serviceAction = ServiceAction.New<IMachineNameProvider>(() => services.AddMachineNameProvider(
+                stringlyTypedMachineNameProviderAction));
+
+            return serviceAction;
+        }
+    }
+}
---
+++ source/R5T.D0028.Default/Code/Services/Implementations/MachineNameProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using R5T.T0007;
+
+using IStringlyTypedMachineNameProvider = R5T.D0027.IMachineNameProvider;
+
+
+namespace R5T.D0028.Default
+{
+    public class MachineNameProvider : IMachineNameProvider
+    {
+        private IStringlyTypedMachineNameProvider StringlyTypedMachineNameProvider { get; }
+
+
+        public MachineNameProvider(IStringlyTypedMachineNameProvider stringlyTypedMachineNameProvider)
+        {
+            this.StringlyTypedMachineNameProvider = stringlyTypedMachineNameProvider;
+        }
+
+        public MachineName GetMachineName()
+        {
+            var machineNameString = this.StringlyTypedMachineNameProvider.GetMachineName();
+
+            var machineName = machineNameString.AsMachineName();
+            return machineName;
+        }
+    }
+}
