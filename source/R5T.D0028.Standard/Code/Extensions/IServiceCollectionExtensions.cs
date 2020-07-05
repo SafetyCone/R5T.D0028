23f708b8670ac653f437c94703197579a25604eb
MinexAutomation <minex.automation@gmail.com>
2020-07-04 23:16:47 -0700

    Added R5T.D0028.Standard library.

---
+++ source/R5T.D0028.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.D0028.Default;
+
+using R5T.Dacia;
+
+using IStringlyTypedMachineNameProvider = R5T.D0027.IMachineNameProvider;
+
+
+namespace R5T.D0028.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static (
+            IServiceAction<IMachineNameProvider> MachineNameProviderAction,
+            IServiceAction<IStringlyTypedMachineNameProvider> StringlyTypedMachineNameProviderAction
+            )
+            AddMachineNameProviderAction(this IServiceCollection services)
+        {
+            var stringlyTypedMachineNameProviderAction = R5T.D0027.Default.IServiceCollectionExtensions.AddMachineNameProviderAction(services);
+
+            var machineNameProviderAction = services.AddMachineNameProviderAction(stringlyTypedMachineNameProviderAction);
+
+            return (
+                machineNameProviderAction,
+                stringlyTypedMachineNameProviderAction);
+        }
+
+        public static IServiceCollection AddMachineNameProvider(this IServiceCollection services)
+        {
+#pragma warning disable IDE0042 // Deconstruct variable declaration
+            var machineNameProviderAction = services.AddMachineNameProviderAction();
+#pragma warning restore IDE0042 // Deconstruct variable declaration
+
+            services.Run(machineNameProviderAction.MachineNameProviderAction);
+
+            return services;
+        }
+    }
+}
